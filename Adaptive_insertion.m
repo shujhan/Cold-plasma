b984bf114b788740c09a465229c9909e8886717d
jiashu han <jiashuhan@MacBook-Pro-410.local>
2023-11-16 19:56:14 -0500

    update

--- Adaptive_insertion.m
+++ Adaptive_insertion.m
@@ -28,6 +28,8 @@ v_passive = zeros(1,N+1);
 alpha = zeros(1,N);
 x = zeros(1,N);
 v = zeros(1,N);
+% weight
+wt = zeros(1,N);
 
 for i = 1:N+1
     alpha_passive(i) = (i-1)/N;
@@ -41,13 +43,18 @@ for i = 1:N
     v(i) = 0;
 end
 
+for i = 1:N
+    wt(i) = alpha_passive(i+1) - alpha_passive(i);
+end
+
+
 % only plot active points 
 x_2 = zeros(1,N);
 v_2 = zeros(1,N);
-x_3 = zeros(1,N);
-v_3 = zeros(1,N);
-x_4 = zeros(1,N);
-v_4 = zeros(1,N);
+% x_3 = zeros(1,N);
+% v_3 = zeros(1,N);
+% x_4 = zeros(1,N);
+% v_4 = zeros(1,N);
 % x_5 = zeros(1,N);
 % v_5 = zeros(1,N);
 % x_6 = zeros(1,N);
@@ -56,11 +63,11 @@ for i = 1:N
     x_2(i) = x(i)+1;
     v_2(i) = v(i);
             
-    x_3(i) = x(i)-1;
-    v_3(i) = v(i);
-
-    x_4(i) = x(i)+2;
-    v_4(i) = v(i);
+    % x_3(i) = x(i)-1;
+    % v_3(i) = v(i);
+    % 
+    % x_4(i) = x(i)+2;
+    % v_4(i) = v(i);
 
     % x_5(i) = x(i)-2;
     % v_5(i) = v(i);
@@ -77,10 +84,10 @@ plot(x,v,'r')
 hold on
 plot(x_2,v_2,'b')
 hold on
-plot(x_3,v_3,'g')
-hold on
-plot(x_4,v_4,'k')
-hold on
+% plot(x_3,v_3,'g')
+% hold on
+% plot(x_4,v_4,'k')
+% hold on
 % plot(x_5,v_5,'m')
 % hold on
 % plot(x_6,v_6,'m')
@@ -96,10 +103,10 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
   hold on
   plot(x_2,v_2,'-b');
   hold on
-  plot(x_3,v_3,'g')
-  hold on
-  plot(x_4,v_4,'k')
-  hold on
+  % plot(x_3,v_3,'g')
+  % hold on
+  % plot(x_4,v_4,'k')
+  % hold on
   % plot(x_5,v_5,'m')
   % hold on
   % plot(x_6,v_6,'m')
@@ -116,8 +123,8 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
         active_num = length(x);
         passive_num = length(x_passive);
 
-        E_field = x_dd(x,x,omega_0,delta,alpha_passive);
-        E_field_passive = x_dd(x_passive(1:passive_num-1),x,omega_0,delta,alpha_passive);
+        E_field = x_dd(x,x,omega_0,delta,wt);
+        E_field_passive = x_dd(x_passive(1:passive_num-1),x,omega_0,delta,wt);
         
 
         if method == 1
@@ -166,11 +173,11 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
             x_2(i) = x(i)+1;
             v_2(i) = v(i);
             
-            x_3(i) = x(i) - 1;
-            v_3(i) = v(i);
-            
-            x_4(i) = x(i) +2;
-            v_4(i) = v(i);
+            % x_3(i) = x(i) - 1;
+            % v_3(i) = v(i);
+            % 
+            % x_4(i) = x(i) +2;
+            % v_4(i) = v(i);
             
             % x_5(i) = x(i) -2;
             % v_5(i) = v(i);
@@ -184,12 +191,12 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
       % plot(x,v); 
       plot(x,v,'-r')
       hold on
-      plot(x_2,v_2,'-b')
-      hold on
-      plot(x_3,v_3,'-g')
-      hold on
-      plot(x_4,v_4,'-k')
-      hold on
+      % plot(x_2,v_2,'-b')
+      % hold on
+      % plot(x_3,v_3,'-g')
+      % hold on
+      % plot(x_4,v_4,'-k')
+      % hold on
       % plot(x_5,v_5,'-m')
       % hold on
       % plot(x_6,v_6,'-m')
@@ -206,11 +213,11 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
           figure(2);subplot(3,2,1+part); 
           plot(x,v,'r')
           hold on
-          plot(x_2,v_2,'b')
-          hold on
-          plot(x_3,v_3,'g')
-          hold on
-          plot(x_4,v_4,'k')
+          % plot(x_2,v_2,'b')
+          % hold on
+          % plot(x_3,v_3,'g')
+          % hold on
+          % plot(x_4,v_4,'k')
           % hold on
           % plot(x_5,v_5,'m')
           % hold on
@@ -230,38 +237,50 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
         count = 1;
         count_passive = 1;
         for i = 1:num_interval
+            a0 = alpha_passive(i);
+            a1 = alpha(i);
+            a2 = alpha_passive(i+1);
+
+            x0 = x_passive(i);
+            x1 = x(i);
+            x2 = x_passive(i+1);
+
+            v0 = v_passive(i);
+            v1 = v(i);
+            v2 = v_passive(i+1);
+
             % euclidean distance in phase sapace
-            dist = sqrt((x_passive(i) - x_passive(i+1))^2 + (v_passive(i) - v_passive(i+1))^2);
+            dist = sqrt((x2-x0)^2 + (v2-v0)^2);
+           
             if (dist > d1)
                 % add first point to passive
-                new_x_passive(count_passive) = x_passive(i);
-                new_alpha_passive(count_passive) = alpha_passive(i);
-                new_v_passive(count_passive) = v_passive(i);
+                new_x_passive(count_passive) = x0;
+                new_alpha_passive(count_passive) = a0;
+                new_v_passive(count_passive) = v0;
                 count_passive = count_passive + 1;
                 
                 % add second point to passive (active point on this interval)
-                new_x_passive(count_passive) = x(i);
-                new_alpha_passive(count_passive) = alpha(i);
-                new_v_passive(count_passive) = v(i);
+                new_x_passive(count_passive) = x1;
+                new_alpha_passive(count_passive) = a1;
+                new_v_passive(count_passive) = v1;
                 count_passive = count_passive + 1;
 
 
                 % insert two active points new_alpha 
-                middle_alpha1 = 0.5*(alpha(i) + alpha_passive(i));
-                middle_alpha2 = 0.5*(alpha(i) + alpha_passive(i+1));
+                middle_alpha1 = 0.5*(a1 + a0);
+                middle_alpha2 = 0.5*(a1 + a2);
                 % use newtons form interpolation
-                a = ((x_passive(i+1) - x(i))/(alpha_passive(i+1) - alpha(i)) - (x(i) - x_passive(i))/(alpha(i) - alpha_passive(i)))/(alpha_passive(i+1)-alpha_passive(i));
-                b = (x(i) - x_passive(i))/(alpha(i) - alpha_passive(i));
-                c = x_passive(i);
-                insert_x1 = a*(middle_alpha1 - alpha_passive(i))*(middle_alpha1 - alpha(i)) + b*(middle_alpha1-alpha_passive(i)) + c;
-                insert_x2 = a*(middle_alpha2 - alpha_passive(i))*(middle_alpha2 - alpha(i)) + b*(middle_alpha2-alpha_passive(i)) + c;
+                a = ((x2-x1)/(a2-a1) - (x1-x0)/(a1-a0))/(a2-a0);
+                b = (x1-x0)/(a1-a0);
+                c = x0;
+                insert_x1 = a*(middle_alpha1 - a0)*(middle_alpha1 - a1) + b*(middle_alpha1-a0) + c;
+                insert_x2 = a*(middle_alpha2 - a0)*(middle_alpha2 - a1) + b*(middle_alpha2-a0) + c;
                 
-                a_v = ((v_passive(i+1) - v(i))/(alpha_passive(i+1) - alpha(i)) - (v(i) - v_passive(i))/(alpha(i) - alpha_passive(i)))/(alpha_passive(i+1)-alpha_passive(i));
-                b_v = (v(i) - v_passive(i))/(alpha(i) - alpha_passive(i));
-                c_v = v_passive(i);
-                insert_v1 = a_v*(middle_alpha1 - alpha_passive(i))*(middle_alpha1 - alpha(i)) + b_v*(middle_alpha1-alpha_passive(i)) + c_v;
-                insert_v2 = a_v*(middle_alpha2 - alpha_passive(i))*(middle_alpha2 - alpha(i)) + b_v*(middle_alpha2-alpha_passive(i)) + c_v;
-                
+                a_v = ((v2-v1)/(a2-a1) - (v1-v0)/(a1-a0))/(a2-a0);
+                b_v = (v1-v0)/(a1-a0);
+                c_v = v0;
+                insert_v1 = a_v*(middle_alpha1 - a0)*(middle_alpha1 - a1) + b_v*(middle_alpha1-a0) + c_v;
+                insert_v2 = a_v*(middle_alpha2 - a0)*(middle_alpha2 - a1) + b_v*(middle_alpha2-a0) + c_v;
                 
                 new_alpha(count) = middle_alpha1;
                 new_x(count) = insert_x1;
@@ -305,11 +324,15 @@ xlabel('x'); ylabel('v'); title(str); axis([ minX maxX vmin vmax])
         v_passive = new_v_passive;
 
         clear new_x new_v new_alpha new_x_passive new_v_passive new_alpha_passive
+
+        for i = 1:num_interval
+            wt(i) = alpha_passive(i+1) - alpha_passive(i);
+        end
     end
 
 
 
-function Efield = x_dd(x_target,x_source,omega_0,delta,alpha_passive)
+function Efield = x_dd(x_target,x_source,omega_0,delta,wt)
     target_num = length(x_target);
     source_num = length(x_source);
     for i = 1:source_num
@@ -324,14 +347,14 @@ function Efield = x_dd(x_target,x_source,omega_0,delta,alpha_passive)
 
     for i = 1:target_num
         for j = 1:source_num
-            Efield(i) = Efield(i) + k(x_target(i),x_source(j),delta)* omega_0*(alpha_passive(j+1)-alpha_passive(j));             
+            Efield(i) = Efield(i) + k(x_target(i),x_source(j),delta)* omega_0*(wt(j));             
         end
     end
 end
 
 
-function weight = k(x,y,delta)
+function kernel = k(x,y,delta)
     c_delta = (1+4*delta^2)^0.5;
-    weight = -c_delta/2*(x-y)/((x-y)^2+delta^2)^0.5+x-y;
+    kernel = -c_delta/2*(x-y)/sqrt((x-y)^2+delta^2)+x-y;
 end
 
